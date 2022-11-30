5b22e76ef17126cc6d6c9beed5c427eac2c69c11
DarkMrAnime <gyllian121@gmail.com>
2022-11-30 13:28:21 +0100

    Fixes

--- resources/views/Sommen/Plus/Basic.blade.php
+++ resources/views/Sommen/Plus/Basic.blade.php
@@ -1,15 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('Sommen/index')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Plus Sommen</title>
-</head>
+@section('Sommen')
+    <!DOCTYPE html>
+    <html lang="en">
 
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Plus Sommen</title>
+    </head>
 
-</body>
+    <body>
 
-</html>
+    </body>
+
+    </html>
+@endsection
