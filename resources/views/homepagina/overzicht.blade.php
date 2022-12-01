c0920c9fa85acd47e202e16cd035f0b619f2b460
Gino Hanemaaijer <g.hanemaaijer11@gmail.com>
2022-12-01 10:59:42 +0100

    aangepast

--- resources/views/homepagina/overzicht.blade.php
+++ resources/views/homepagina/overzicht.blade.php
@@ -1,25 +1,10 @@
 <style>
-.block{
-    margin-left: 2.5%;
-            box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
-            border-radius: 20px;
-            max-width: 95%;
-            padding-top: 40px;
-            padding-bottom: 40px;
-            margin-bottom: -290px;
-            background-color: #fff;
-            position: relative;
-            height: 150%;
 
-}
 
 </style>
 
 
 
 <body>
-    <div class="block">
 
-
-    </div>
 </body>
