3af7f332c3ece2cc0ad8f52c9a6ecfbc2c1ae77f
belrace <60099664+belrace@users.noreply.github.com>
2022-12-05 12:56:00 +0100

    plus en keer

---
+++ resources/views/Sommen/Times/expert.blade.php
@@ -0,0 +1,44 @@
+@extends('Sommen/index')
+
+@section('Sommen')
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Keer Sommen</title>
+    </head>
+
+    <body>
+    
+     <?php
+    $value1 = rand(20, 100);
+    $value2 = rand(0, $value1);
+    $value3 = $value1 * $value2;
+    
+    echo $value1;
+    echo ' X ';
+    echo $value2;
+    echo ' = ';
+    echo $value3;
+    ?>
+    <form>
+    Antwoord: <input type="number" id="ant" name="ant"><br>
+    
+    <input type="submit"></br>
+
+    </form>
+
+
+    <?php
+    
+
+
+    ?>
+
+    </body>
+
+    </html>
+@endsection
